b642ccd4facb9a62bd94b75b5d3aa2586f87555f
Rich Xu <richardxu100@gmail.com>
2016-07-10 12:58:27 +0800

    Kind of configured Routes.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import ReactRouter from 'react-router';
+import Main from '../containers/Main';
+const Router = ReactRouter.Router;
+const Route = ReactRouter.Route;
+const hashHistory = ReactRouter.hashHistory;
+const IndexRoute = ReactRouter.IndexRoute;
+
+export const Routes = ( //check if this works
+	<Router history="hashHistory">
+		<Route path="/" component={Main}>
+			
+		</Route>
+	</Router>
+);
